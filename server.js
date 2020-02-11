4a3aee3f144ef3094564e9ef307dbbead8575bf4
Patrick <patrick204nqh@gmail.com>
2020-02-11 13:26:48 +0700

    structure my application patrickApp-1

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+
+app.use(express.static('public'))
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
 })
- 
+app.get('/user', function (req, res) {
+  res.send('Hello')
+})
 app.listen(port, ()=> {
     console.log("Server running on port " + port)
 })
